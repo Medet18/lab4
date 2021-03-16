ac2047ebc44301d8c04dceb0e3995836a9eb5997
Medet <medetkurganbayev18@gmail.com>
2021-03-16 18:08:14 +0600

    clients tests

---
+++ tests/Feature/ClientTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ClientTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+    //1
+    public function test_client_id_get_request(){
+        $response = $this->get('/client/1');
+        $response->assertStatus(200);    
+    }
+    //2
+    public function test_get_unexisted_client(){
+        $response = $this->get('/client/1000');
+        $response->assertStatus(404);
+    }
+    //3
+    public function test_client_response(){
+        $response = $this->get('/client/1');
+        $response = assertViewHas('client');
+    }
+
+}
